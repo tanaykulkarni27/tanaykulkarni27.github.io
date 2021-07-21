31debd1e2687dede6eaf1d8988e7ef621f812b96
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2021-07-21 21:38:00 +0530

    Add files via upload

---
+++ json_parsing/index.js
@@ -0,0 +1,20 @@
+function get_json(url) {
+   url = 'https://cors-anywhere.herokuapp.com/'+url;
+//    const axios = require('axios').default;
+   axios.get(url)
+        .then(function (response) {
+        // handle success
+        console.log(response);
+        })
+        .catch(function (error) {
+        // handle error
+        console.log(error);
+        })
+        .then(function () {
+        // always executed
+        });
+}
+function load(){
+        get_json("https://appfeeds.moneycontrol.com/jsonapi/market/marketmap&format=&type=0&ind_id=9");
+   
+}
